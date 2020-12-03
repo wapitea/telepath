e5408fd137fc88c02050e27372068bedc6bbdd46
Alexandre Lepretre <alexandre.lprtr@gmail.com>
2020-12-03 16:54:43 -0500

    Release 0.1.3

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Telepath.MixProject do
   def project do
     [
       app: :telepath,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
